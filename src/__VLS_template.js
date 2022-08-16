9210f45e70b52b0bd1b60bb951b79f36b8ce4612
Rafaella <ellaatanazio@gmail.com>
2022-08-16 15:40:04 -0300

    desafiofrontend_somos

---
+++ src/__VLS_template.js
@@ -0,0 +1,21 @@
+// import axios from "axios";
+
+// export default (await import("vue")).defineComponent({
+//   name: "App",
+//   components: {},
+
+//   data: () => {
+//     return {
+//       pokemons: [],
+//     };
+//   },
+//   mounted() {
+//     axios
+//       .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+//       .then((response) => {
+//         console.log("response");
+//         this.pokemons = response.data.results;
+//       });
+//   },
+// });
+// const __VLS_template = () => ({});
